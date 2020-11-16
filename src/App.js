8eeef730750048beb36d9a1eb688289de1b69e73
gkotecki <gabrielkotecki@gmail.com>
2020-11-16 01:09:05 -0300

    ADD: blinking rectangle with fps counter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React from docker!
-        </a>
-      </header>
-    </div>
-  );
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    let secondsPassed = 0;
+    let oldTimeStamp = 0;
+    let fps;
+
+    const gameLoop = (timeStamp) => {
+      const canvas = canvasRef.current;
+      canvas.width = window.innerWidth * 2;
+      canvas.height = window.innerHeight * 2;
+      canvas.style.width = `${window.innerWidth}px`;
+      canvas.style.height = `${window.innerHeight}px`;
+
+      const context = canvas.getContext("2d");
+      context.scale(2, 2);
+
+      // Calculate the number of seconds passed since the last frame
+      secondsPassed = (timeStamp - oldTimeStamp) / 1000;
+      oldTimeStamp = timeStamp;
+
+      // Calculate fps
+      fps = Math.round(1 / secondsPassed);
+
+      // Draw number to the screen
+      context.fillStyle = "white";
+      context.fillRect(0, 0, 200, 100);
+      context.font = "25px sans-serif";
+      context.fillStyle = "black";
+      context.fillText("FPS: " + fps, 10, 30);
+
+      update(context, secondsPassed);
+
+      draw(context);
+
+      requestAnimationFrame(gameLoop);
+    };
+    gameLoop();
+  }, []);
+
+  function update(context, secondsPassed) {
+    let randomColor = Math.random() > 0.5 ? "#ff8080" : "#0099b0";
+    context.fillStyle = randomColor;
+  }
+
+  function draw(context) {
+    context.fillRect(100, 50, 200, 175);
+  }
+
+  return <canvas ref={canvasRef} />;
 }
 
 export default App;
