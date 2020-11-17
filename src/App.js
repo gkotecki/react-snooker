c9e3e15cafba63026abf9b66983e8cebe932c899
gkotecki <gabrielkotecki@gmail.com>
2020-11-16 23:30:23 -0300

    UPDATE: separating in functions

--- src/App.js
+++ src/App.js
@@ -6,47 +6,46 @@ function App() {
   useEffect(() => {
     let secondsPassed = 0;
     let oldTimeStamp = 0;
-    let fps;
 
     const gameLoop = (timeStamp) => {
-      const canvas = canvasRef.current;
-      canvas.width = window.innerWidth * 2;
-      canvas.height = window.innerHeight * 2;
-      canvas.style.width = `${window.innerWidth}px`;
-      canvas.style.height = `${window.innerHeight}px`;
+      const ctx = getContext();
+      
+      ({ secondsPassed, oldTimeStamp } = drawFps(secondsPassed, timeStamp, oldTimeStamp, ctx));
 
-      const context = canvas.getContext("2d");
-      context.scale(2, 2);
-
-      // Calculate the number of seconds passed since the last frame
-      secondsPassed = (timeStamp - oldTimeStamp) / 1000;
-      oldTimeStamp = timeStamp;
-
-      // Calculate fps
-      fps = Math.round(1 / secondsPassed);
-
-      // Draw number to the screen
-      context.fillStyle = "white";
-      context.fillRect(0, 0, 200, 100);
-      context.font = "25px sans-serif";
-      context.fillStyle = "black";
-      context.fillText("FPS: " + fps, 10, 30);
-
-      update(context, secondsPassed);
-
-      draw(context);
+      update(ctx, secondsPassed);
 
       requestAnimationFrame(gameLoop);
     };
     gameLoop();
   }, []);
 
+  function drawFps(secondsPassed, timeStamp, oldTimeStamp, ctx) {
+    // Calculate the number of seconds passed since the last frame
+    secondsPassed = (timeStamp - oldTimeStamp) / 1000;
+    oldTimeStamp = timeStamp;
+
+    // Draw number to the screen
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, 200, 100);
+    ctx.font = "25px sans-serif";
+    ctx.fillStyle = "black";
+    ctx.fillText("FPS: " + Math.round(1 / secondsPassed), 10, 30);
+    return { secondsPassed, oldTimeStamp };
+  }
+
+  function getContext() {
+    const canvas = canvasRef.current;
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    canvas.style.width = `${canvas.width}px`;
+    canvas.style.height = `${canvas.height}px`;
+
+    return canvas.getContext("2d");
+  }
+
   function update(context, secondsPassed) {
     let randomColor = Math.random() > 0.5 ? "#ff8080" : "#0099b0";
     context.fillStyle = randomColor;
-  }
-
-  function draw(context) {
     context.fillRect(100, 50, 200, 175);
   }
 
