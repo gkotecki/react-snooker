796a190ebea43cac0706479b4a332de1d2347bd7
gkotecki <gabrielkotecki@gmail.com>
2020-11-17 01:05:37 -0300

    UPDATE: styling, cleanup, initial pool table with fps counter

--- src/App.tsx
+++ src/App.tsx
@@ -3,55 +3,65 @@ import React, { useRef, useEffect } from "react";
 function App() {
   const canvasRef = useRef(null);
 
+  /**
+   * React update hook
+   */
   useEffect(() => {
+    // Set parameters to be reused within hook state
     let secondsPassed = 0;
     let oldTimeStamp = 0;
+    let fps = 0;
 
-    console.log();
-
+    // Start game loop
     const gameLoop = (timeStamp) => {
-      const ctx = getContext();
-      
-      ({ secondsPassed, oldTimeStamp } = drawFps(secondsPassed, timeStamp, oldTimeStamp, ctx));
+      const ctx: CanvasRenderingContext2D = canvasRef.current.getContext("2d");
 
-      update(ctx, secondsPassed);
+      // Calculate the number of seconds passed since the last frame
+      ({ fps, secondsPassed, oldTimeStamp } = calculateFps( secondsPassed, timeStamp, oldTimeStamp ));
+
+      updateCanvas(ctx, fps);
 
       requestAnimationFrame(gameLoop);
     };
     gameLoop(console.timeStamp);
   }, []);
 
-  function drawFps(secondsPassed, timeStamp, oldTimeStamp, ctx) {
-    // Calculate the number of seconds passed since the last frame
+  /**
+   * Calculates FPS and set time parameters
+   */
+  function calculateFps(secondsPassed: number, timeStamp: any, oldTimeStamp: number) {
     secondsPassed = (timeStamp - oldTimeStamp) / 1000;
     oldTimeStamp = timeStamp;
+    const fps = Math.round(1 / secondsPassed);
+    return { fps, secondsPassed, oldTimeStamp };
+  }
 
-    // Draw number to the screen
-    ctx.fillStyle = "white";
-    ctx.fillRect(0, 0, 200, 100);
-    ctx.font = "25px sans-serif";
+  /**
+   * Clears previous frames and draws new content
+   */
+  function updateCanvas(ctx: CanvasRenderingContext2D, fps: number) {
+    // Clears previous frame
+    ctx.clearRect(0, 0, 500, 700);
+
+    // Draw FPS string
+    ctx.font = "14px monospace";
     ctx.fillStyle = "black";
-    ctx.fillText("FPS: " + Math.round(1 / secondsPassed), 10, 30);
-    return { secondsPassed, oldTimeStamp };
+    ctx.fillText("FPS: " + fps, 0, 11);
+
+    // Draw billiards table
+    ctx.fillStyle = "#420";
+    ctx.fillRect(25, 25, 450, 650);
+    ctx.fillStyle = "#090";
+    ctx.fillRect(50, 50, 400, 600);
+
+    // Draw white ball
+    ctx.fillStyle = "#fff"
+    ctx.beginPath();
+    ctx.arc(250, 350, 12, 0, 2*Math.PI);
+    ctx.fill();
   }
 
-  function getContext() {
-    const canvas = canvasRef.current;
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    canvas.style.width = `${canvas.width}px`;
-    canvas.style.height = `${canvas.height}px`;
-
-    return canvas.getContext("2d");
-  }
-
-  function update(context, secondsPassed) {
-    let randomColor = Math.random() > 0.5 ? "#ff8080" : "#0099b0";
-    context.fillStyle = randomColor;
-    context.fillRect(100, 50, 200, 175);
-  }
-
-  return <canvas ref={canvasRef} />;
+  return <canvas ref={canvasRef} width="500" height="700" />;
 }
 
 export default App;
